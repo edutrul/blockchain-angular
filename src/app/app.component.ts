0a623f1f14b4807a096a4f0cb95bc2162878f867
giampieer <giampieer24@gmail.com>
2018-08-31 00:08:50 -0500

    create certificate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './certificate/certificate.component.html',
+  styleUrls: ['./certificate/certificate.component.css']
 })
 export class AppComponent {
   title = 'blockchain';
