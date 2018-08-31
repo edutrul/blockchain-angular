46a34e5474da170740c2f48e9d7bc8659c8b533b
giampieer <giampieer24@gmail.com>
2018-08-30 23:26:43 -0500

    create certificate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CertificateComponent } from './certificate/certificate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertificateComponent
   ],
   imports: [
     BrowserModule
